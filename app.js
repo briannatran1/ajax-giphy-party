6c21fdbd6943c8263472880df9302c761dd73e99
Jung-GunSong <jsong781@gmail.com>
2023-08-15 19:30:20 -0400

    working on part 1

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-$(mainFunction); //loads DOM
+// $(mainFunction); //loads DOM
+$(function() {
+  async function retrieveGif(event){
+    event.preventDefault();
+    let userSearch = $('#search').val();
+    console.log(userSearch);
+    const response = await fetch("http://api.giphy.com/v1/gifs/search", {
+      api_key: "CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh",
+      q: `${userSearch}`
+    });
 
-async function retriveGif(){
-  let userSearch = $('#search').val();
-  const response = await fetch('http://api.giphy.com/v1/gifs/search', {
-    api_key: CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh,
-    q: userSearch
-  });
-  const gif = await response.img();
+    console.log(response);
 
-  console.log('response = ', response);
-}
+  }
 
-$('#search').on('click', retriveGif);
+  $('#search').on('click', retrieveGif);
+})
 
 
-console.log("Let's get this party started!");
+
+
+// console.log("Let's get this party started!");
 
 // api key = CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh
