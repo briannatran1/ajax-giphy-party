757a940bfce58da9030db04382f5fd8fe2e20b03
Brianna Tran <briannatran45@gmail.com>
2023-08-15 16:10:01 -0700

    added giphy api

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+$(mainFunction); //loads DOM
+
+async function retriveGif(){
+  let userSearch = $('#search').val();
+  const response = await fetch('http://api.giphy.com/v1/gifs/search', {
+    api_key: CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh,
+    q: userSearch
+  });
+  const gif = await response.img();
+
+  console.log('response = ', response);
+}
+
+$('#search').on('click', retriveGif);
+
+
 console.log("Let's get this party started!");
+
+// api key = CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh
