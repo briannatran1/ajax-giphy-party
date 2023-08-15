44d3eeed9b99de3feaa325d194b3ca6ee925cc93
Brianna Tran <briannatran45@gmail.com>
2023-08-15 16:50:25 -0700

    fixed fetch bug

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // $(mainFunction); //loads DOM
 $(function() {
+  let $gifArea = $('.gif-area');
+
+
   async function retrieveGif(event){
     event.preventDefault();
+
     let userSearch = $('#search').val();
-    console.log(userSearch);
-    const response = await fetch("http://api.giphy.com/v1/gifs/search", {
-      api_key: "CuYWrRsOGdG3YNUuCgjavI1QueJ1bYEh",
-      q: `${userSearch}`
-    });
+    let api_key = "qVuTy0qY9wfLZxCn5E8ph4LVWchxe62q";
+    let q = `${userSearch}`;
+
+    const params = new URLSearchParams({api_key, q})
+    let response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
 
     console.log(response);
 
   }
 
   $('#search').on('click', retrieveGif);
+
+  $('#remove').on('click', function(){
+    $gifArea.empty();
+  });
 })
 
 
